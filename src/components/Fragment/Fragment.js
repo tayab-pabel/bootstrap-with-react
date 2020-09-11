3d6726433ed5220f7a77a31e350f545a45e289fd
Tayab Pabel <67518363+tayab-pabel@users.noreply.github.com>
2020-09-11 08:44:16 +0600

    Add files via upload

---
+++ src/components/Fragment/Fragment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Fragment = () => {
+  return (
+    <div>
+      <React.Fragment>
+        <td>Hello</td>
+        <td>World</td>
+      </React.Fragment>
+    </div>
+  );
+};
+
+export default Fragment;
